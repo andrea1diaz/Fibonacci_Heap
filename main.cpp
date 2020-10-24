b1d40823a295ce618ab0ac60b985262584a9523c
icyhot-ultra <icyhot.ultra@gmail.com>
2020-10-23 19:50:09 -0500

    new main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-
-#include "FibonacciHeap.hpp"
+#include "Graph.h"
 
 int main () {
-	FibonacciHeap<int> bh;
+	// FibonacciHeap<int> bh;
 
-	// TODO: Revisar que este bien
-    for (int i = 1; i <= 100; ++i) {
-         bh.insert(i);
-     }
-	bh.extract_min();
-	bh.extract_min();
-    bh.extract_min();
-    bh.extract_min();
-    bh.extract_min();
-    bh.extract_min();
-    bh.extract_min();
-	bh.print_heap();
-	// End test
+	// // TODO: Revisar que este bien
+    // for (int i = 1; i <= 100; ++i) {
+    //      bh.insert(i);
+    //  }
+	// bh.extract_min();
+	// bh.extract_min();
+    // bh.extract_min();
+    // bh.extract_min();
+    // bh.extract_min();
+    // bh.extract_min();
+    // bh.extract_min();
+	// bh.print_heap();
+	// // End test
 
-	// Lectura de imagenes
-	bh.buildFromInput();
+	// // Lectura de imagenes
+	// bh.buildFromInput();
+	Graph* g = new Graph();
 	return 0;
 }
